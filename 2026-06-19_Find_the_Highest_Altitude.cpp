44fac07599c4e3d7347dedcd69c8ddf93fe62279
Anu762002 <anubhav762002@gmail.com>
2026-06-19 11:25:44 +0530

    solve: Find the Highest Altitude

---
+++ 2026-06-19_Find_the_Highest_Altitude.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n=gain.size();
+        int ans=0;
+        ans=max(ans,gain[0]);
+        for(int i=1;i<n;i++){
+            gain[i]+=gain[i-1];
+            ans=max(ans,gain[i]);
+        }
+        return ans;
+    }
+};
