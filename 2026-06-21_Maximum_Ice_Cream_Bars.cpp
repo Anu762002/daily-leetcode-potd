c4aeb1bf49b7be90b193a6919c58a2d70a19094a
Anu762002 <anubhav762002@gmail.com>
2026-06-21 07:55:42 +0530

    solve: Maximum Ice Cream Bars

---
+++ 2026-06-21_Maximum_Ice_Cream_Bars.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int cnt=0;
+        sort(costs.begin(),costs.end());
+        for(auto it : costs){
+            if(it<=coins){
+                cnt++;
+                coins-=it;
+            }
+            else break;
+        }
+        return cnt;
+    }
+};
