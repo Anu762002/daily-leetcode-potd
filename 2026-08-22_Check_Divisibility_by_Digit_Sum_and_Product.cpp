3088b793d2105ec96dc7a1c70367a89375b1fbbe
Anu762002 <anubhav762002@gmail.com>
2026-08-23 21:54:32 +0530

    solve: Check Divisibility by Digit Sum and Product

---
+++ 2026-08-22_Check_Divisibility_by_Digit_Sum_and_Product.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum=0;
+        int temp=n;
+        int prod=1;
+        while(temp){
+            sum+=temp%10;
+            prod*=temp%10;
+            temp/=10;
+        }
+        return n%(sum+prod)==0;
+    }
+};
